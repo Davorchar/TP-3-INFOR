a056caf2c5bba2278bc9a229534f9c0f8c19642c
davorchar <danrdorcharchir@hotmail.com>
2024-06-09 15:26:15 -0300

    Primer Commit

---
+++ Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Dim dato1 As Integer
+    Dim factorial As Integer = 1
+
+    Private Sub cmdEjecutar_Click(sender As Object, e As EventArgs) Handles cmdEjecutar.Click
+        If Integer.TryParse(txtIngresarDat.Text, dato1) Then
+            For cuenta As Integer = 1 To dato1
+                factorial *= cuenta
+
+            Next
+            lblMostrar.Text = "el factorial de" & dato1.ToString() & " es " & factorial.ToString()
+        End If
+    End Sub
+End Class
