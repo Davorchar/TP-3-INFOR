fa0214d63398c8af8427061567150ee9ca4b15f7
davorchar <danrdorcharchir@hotmail.com>
2024-06-12 00:39:12 -0300

    tercer commit, acts b

---
+++ Ejercicio5Codigo.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+
+    Private Sub cmdEjecutar_Click(sender As Object, e As EventArgs) Handles cmdEjecutar.Click
+        Dim num, num2 As Integer
+        Dim primo As Boolean = True
+        If Integer.TryParse(txtIngresar.Text, num) Then
+            Dim mitad As Integer = num / 2
+            For num2 = 2 To mitad
+                If num Mod num2 = 0 Then
+                    primo = False
+                    Exit For
+                End If
+            Next
+            If primo And num > 1 Then
+                lblMostrar.Text = num & "es primo"
+            Else
+                lblMostrar.Text = num & "no es primo"
+            End If
+        End If
+    End Sub
+End Class
---
+++ ejercicio 7.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private Sub cmdEjecutar_Click(sender As Object, e As EventArgs) Handles cmdEjecutar.Click
+        Dim rnd As New Random
+        Dim num, posMayor As Integer
+        Dim suma As Integer = 0
+        Dim mayor As Integer = 0
+        For cuenta = 1 To 20 Step 1
+            num = rnd.Next(1, 51)
+            If num Mod 2 = 0 Then
+                suma = suma + 1
+            End If
+            If num > mayor Then
+                mayor = num
+                posMayor = cuenta
+            End If
+            ListBox1.Items.Add(num)
+        Next
+        lblMostrar.Text = suma & " son pares"
+        lblMostrar2.Text = "el mayor es " & mayor
+        lblMostrar3.Text = "en la posicion " & posMayor
+    End Sub
+End Class
